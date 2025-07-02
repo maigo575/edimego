a44369bfba6659b2ec91ce89ce80c507c764ad17
太平優香 <maigo@taiheiyuukanoMacBook-Pro.local>
2025-07-02 16:10:07 +0900

    ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:penname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:penname])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
