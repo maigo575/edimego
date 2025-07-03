5d84f01721f96dfd3ea0aa936ce09c0cc76f73ab
太平優香 <maigo@taiheiyuukanoMacBook-Pro.local>
2025-07-03 17:44:17 +0900

    ログアウト機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "works/show"
   get "works/new"
   get "works/create"
-  devise_for :users
+  devise_for :users, sign_out_via: :delete
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
